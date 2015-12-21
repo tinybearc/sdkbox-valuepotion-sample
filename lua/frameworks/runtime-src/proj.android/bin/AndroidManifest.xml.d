7928431bf245dfbdc795369d9353ef489610851c
hugo <hugohuang1111@gmail.com>
2015-12-21 14:23:05 +0800

    remove useless file

--- lua/frameworks/runtime-src/proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/qeeplay/Documents/chukong_test/sdkbox-pre-valuepotion/lua/frameworks/runtime-src/proj.android/bin/AndroidManifest.xml : \
-/Users/qeeplay/Documents/chukong_test/sdkbox-pre-valuepotion/lua/frameworks/runtime-src/proj.android/AndroidManifest.xml \
